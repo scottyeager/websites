98d692599bfd4113bb5ad9dfcce9902dddd8c947
kristof <kristof@incubaid.com>
2020-05-21 10:37:19 +0200

    isntall

---
+++ docs/blogs.md
@@ -0,0 +1,2 @@
+see https://github.com/threebotserver/publishingtools/blob/development/docs/blog.md#example-entry-in-the-config-toml
+
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#TODO: start the local web tools
+tfweb -c config.toml
 
 #TODO: need to add conscious internet, prob on other port 3001?
 #question: can we include pages from conscious internet repo in wiki?
