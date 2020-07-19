2575cd8e7705d23ac933b12a00ee26691b8a13f7
Bishoy <abdelmab@codescalers.com>
2020-07-19 23:13:15 +0200

    fix starting tfweb

--- tools/start_tfweb.sh
+++ tools/start_tfweb.sh
@@ -3,4 +3,4 @@ if ! [ -x "$(command -v tfweb)" ]; then
 echo 'Error: tfweb has not been installed' >&2
 exit 1
 fi
-cd ~/code/github/publishingtools &&  tfweb -c ~/code/github/threefoldfoundation/websites/config.toml
+tfweb -c ~/code/github/threefoldfoundation/websites/config.toml
