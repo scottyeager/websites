3949ecadbd3e5904ca364692d9c3d3bdc1de7172
kristof de spiegeleer <kristof@greenitglobe.com>
2020-07-19 09:21:47 +0200

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set +ex
+set -ex
 
 # this script to install tfweb and conscious_internet
 # tfweb port is 3000
@@ -50,7 +50,7 @@ if [ -d "$DEST/websites" ] ; then
 else
     mkdir -p $DEST
     cd $DEST
-    git clone --depth 1 "https://github.com/threefoldfoundation/websites"    
+    git clone --depth=1 "https://github.com/threefoldfoundation/websites"    
 fi
 
 cd $DEST/websites
